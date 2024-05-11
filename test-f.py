c2f38f0ec712176939d50854023edd0f145457b3
Gane807405 <168809854+Gane807405@users.noreply.github.com>
2024-05-11 21:37:09 +0530

    Update test-f.py

--- test-f.py
+++ test-f.py
@@ -2,3 +2,4 @@ import streamlit as st
 import streamlit.components as components
 media_url="https://www.instagram.com/p/C4YGDd0y5xC/"
 components.v1.html(f'<blockquote class="instagram-media" data-instgrm-permalink="{media_url}" data-instgrm-version="13"></blockquote><script async src="//www.instagram.com/embed.js"></script>', width=400, height=400, scrolling=True)
+st.write("hello")
